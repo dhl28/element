df295d558e007d67c2ce87567f1fff9c66e23884
douhongliang <dhl28@126.com>
2019-10-15 09:29:50 +0800

    添加lib文件夹

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,15 +1,16 @@
 <template>
   <div style="margin: 20px;">
     <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <el-date-picker showWeekNumber v-model="value1" type="datetime" placeholder="选择日期时间"></el-date-picker>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        input: 'Hello Element UI!'
-      };
-    }
-  };
+export default {
+  data() {
+    return {
+      input: "Hello Element UI!"
+    };
+  }
+};
 </script>
